09bca216181eee50de5d5ba2340c33a1f6505797
Shun Git <kwokshungit@gmail.com>
2017-06-29 09:36:18 +0900

    use new API for bookmarklet

--- fc_bookmarklet_loader.js
+++ fc_bookmarklet_loader.js
@@ -1,9 +1,5 @@
 javascript:(
   function () {
-    var js = document.createElement('script');
-    js.setAttribute('type', 'text/javascript');
-    js.setAttribute('id', 'frozenCookieScript');
-    js.setAttribute('src', 'https://rawgithub.com/haerik/FrozenCookies/master/frozen_cookies.js');
-    document.head.appendChild(js);
+    Game.LoadMod('https://rawgit.com/randname/FrozenCookies/master/frozen_cookies.js');
  }()
 );
